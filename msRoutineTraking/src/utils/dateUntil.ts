21cda3e8876a814eb1b37873c6372ffdd075971c
xpecter <mr.xpecterly@gmail.com>
2025-02-24 16:45:12 -0500

    update date format dd/MM/yyyy hh:mm

--- msRoutineTraking/src/utils/dateUntil.ts
+++ msRoutineTraking/src/utils/dateUntil.ts
@@ -7,5 +7,5 @@ export const getCurrentDate = () => {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
   
-    return `${day},${month},${year} ${hours}:${minutes}`;
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
